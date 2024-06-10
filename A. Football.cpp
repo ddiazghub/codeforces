58ad7078c1eca9ca66a9eb99681c5f5ca771288f
ddiazghub <dedemoya@uninorte.edu.co>
2024-06-09 21:56:10 -0500

    Football

--- A. Football.cpp
+++ A. Football.cpp
@@ -1,34 +1,99 @@
 // Problem: A. Football
-// Contest: Codeforces Beta Round 77 (Div. 2 Only)
+// Contest: Codeforces Beta Round 42 (Div. 2)
 // Judge: Codeforces
-// URL: https://codeforces.com/problemset/problem/96/A
+// URL: https://codeforces.com/problemset/problem/43/A
 // Memory Limit: 256
 // Time Limit: 2000
-// Start: Sat May  4 20:35:52 2024
+// Start: Sun Jun  9 21:25:14 2024
 
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-  string input;
-  int consecutive = 1;
-  cin >> input;
+typedef long long i64;
+typedef int i32;
 
-  for (int i = 1; i < input.size(); i++) {
-    if (input[i - 1] == input[i]) {
-      consecutive++;
+const i32 I32_MAX = numeric_limits<i32>().max();
+const i64 I64_MAX = numeric_limits<i64>().max();
 
-      if (consecutive == 7) {
-        cout << "YES" << endl;
-        return 0;
-      }
-    } else {
-      consecutive = 1;
-    }
+const string YES = "YES";
+const string NO = "NO";
+
+template <typename T> inline void print_array(T arr, size_t n, string end = "", string separator = " ") {
+  for (int i = 0; i < n - 1; i++) {
+    cout << arr[i] << separator;
   }
 
-	cout << "NO" << endl;
+  cout << arr[n - 1] << end << endl;
+}
+
+template <typename T> inline void print(vector<T> &arr) { print_array(arr, arr.size()); }
+template <typename T, size_t S> inline void print(array<T, S> &arr) { print_array(arr, arr.size()); }
+template <typename T> inline void print(T arr[], size_t n) { print_array(arr, n); }
+
+template <typename T> inline void dbg_array(T arr, size_t n) {
+  cout << '[';
+
+  print_array(arr, n, "]", ", ");
+}
+template <typename T> inline void dbg_map(T m) {
+  cout << '{' << endl;
+
+  for (auto [key, value]: m) {
+    cout << "  " << key << ": " << value << endl;
+  }
+
+  cout << '}' << endl;
+}
+
+template <typename T> inline void dbg_set(T s) {
+  cout << '{';
+
+  for (auto value: s) {
+    cout << value << ", ";
+  }
+
+  cout << '}' << endl;
+}
+template <typename T1, typename T2> inline void dbg(pair<T1, T2> pair) {
+  cout << '(' << pair.first << ", " << pair.second << ')' << endl;
+}
+template <typename T> inline void dbg(vector<T> &arr) { dbg_array(arr, arr.size()); }
+template <typename T, size_t S> inline void dbg(array<T, S> &arr) { dbg_array(arr, arr.size()); }
+template <typename T> inline void dbg(T arr[], size_t n) { dbg_array(arr, n); }
+template <typename K, typename V> inline void dbg(map<K, V> m) { dbg_map(m); }
+template <typename K, typename V> inline void dbg(unordered_map<K, V> m) { dbg_map(m); }
+template <typename T> inline void dbg(set<T> s) { dbg_set(s); }
+template <typename T> inline void dbg(unordered_set<T> s) { dbg_set(s); }
+
+string solve() {
+  int n;
+  cin >> n;
+  map<string, int> goals;
+
+  for (int i = 0; i < n; i++) {
+    string team;
+    cin >> team;
+    goals[team]++;
+  }
+
+  auto team1 = goals.cbegin();
+  auto team2 = next(goals.cbegin(), 1);
+    
+  if (goals.size() == 1)
+    return team1->first;
+
+  if (team1->second > team2->second)
+    return team1->first;
+  else
+    return team2->first;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin.clear();
+  cout << solve() << endl;
 
 	return 0;
 }
